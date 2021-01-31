02d29096ea40c8d926ccc27bb9ba386fe2b278d4
Josh Morrison <josh.morrison1999@gmail.com>
2021-01-31 09:28:40 -0500

    added the ability for the user to press their keyboard and it will show in the piano ui in unity

--- Assets/PianoUI.cs
+++ Assets/PianoUI.cs
@@ -2,9 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 using TMPro;
 
 public class PianoUI : MonoBehaviour
 {
     public Image[] PianoKeys;
+    public List<Image> currentPressedNotes;
+
+    void Start()
+    {
+        InputSystem.onDeviceChange += (device, change) =>
+        {
+            if (change != InputDeviceChange.Added) return;
+
+            var midiDevice = device as Minis.MidiDevice;
+            if (midiDevice == null) return;
+
+            midiDevice.onWillNoteOn += (note, velocity) => {
+                PianoKeyPressedUI(note.shortDisplayName);
+            };
+
+            midiDevice.onWillNoteOff += (note) => {
+                PianoKeyLiftedUI(note.shortDisplayName);
+            };
+        };
+    }
+
+    void PianoKeyPressedUI(string notePressed)
+    {
+        foreach (Image each in PianoKeys)
+        {
+            if (each.name == notePressed)
+            {
+                currentPressedNotes.Add(each);
+                each.color = Color.red;
+            }
+        }
+    }
+
+    void PianoKeyLiftedUI(string notePressed)
+    {
+        foreach (Image each in PianoKeys)
+        {
+            if (each.name == notePressed)
+            {
+                currentPressedNotes.Remove(each);
+                each.color = Color.white;
+            }
+        }
+    }
+
+
 }
