9caf2b0b9fce24926653aab68efe1a7e4a7ab99b
Josh Morrison <josh.morrison1999@gmail.com>
2021-03-27 16:05:15 -0400

    changed and added new things to the settings menu and main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -15,22 +15,32 @@ public class MainMenu : MonoBehaviour
 	public GameObject SongSelectionPanel;
 	public GameObject SettingsMenuPanel;
 
-	public TextMeshProUGUI playerLevelUI;
-	public TextMeshProUGUI playerMoneyUI;
+	public TextMeshProUGUI SongSelection_playerLevelUI;
+	public TextMeshProUGUI SongSelection_playerMoneyUI;
+	public Slider SongSelection_playerExpSliderUI;
 
-	public Slider playerExpSliderUI;
+	public TextMeshProUGUI Settings_playerLevelUI;
+	public TextMeshProUGUI Settings_playerMoneyUI;
+	public Slider Settings_playerExpSliderUI;
+
+	public TextMeshProUGUI MainMenu_playerLevelUI;
+	public TextMeshProUGUI MainMenu_playerMoneyUI;
+	public Slider MainMenu_playerExpSliderUI;
+
+
 
 	void Start()
 	{
-		// Player persistent data UI
-		playerLevelUI.text = PersistentData.data.level.ToString();
-		playerMoneyUI.text = PersistentData.data.money.ToString();
+
+		SettingsMenuPanel.SetActive(false);
+		SongSelectionPanel.SetActive(false);
+
+		PlayerStatsLoad();
 
 		Button btn = songSelectionBtn.GetComponent<Button>();
 		btn.onClick.AddListener(songSelectionClicked);
 
 		EXPSlideSetup();
-		MoneyTextSetup();
 	}
 
 	public void showMainMenu()
@@ -45,22 +55,49 @@ public class MainMenu : MonoBehaviour
 
 	private void EXPSlideSetup()
     {
-		playerExpSliderUI.maxValue = ReturnXPNeededToLevelUp(PersistentData.data.level);
-		playerExpSliderUI.minValue = 0;
-		playerExpSliderUI.value = PersistentData.data.exp;
+		SongSelection_playerExpSliderUI.maxValue = ReturnXPNeededToLevelUp(PersistentData.data.level);
+		SongSelection_playerExpSliderUI.minValue = 0;
+		SongSelection_playerExpSliderUI.value = PersistentData.data.exp;
 
-    }
+		Settings_playerExpSliderUI.maxValue = ReturnXPNeededToLevelUp(PersistentData.data.level);
+		Settings_playerExpSliderUI.minValue = 0;
+		Settings_playerExpSliderUI.value = PersistentData.data.exp;
+
+		MainMenu_playerExpSliderUI.maxValue = ReturnXPNeededToLevelUp(PersistentData.data.level);
+		MainMenu_playerExpSliderUI.minValue = 0;
+		MainMenu_playerExpSliderUI.value = PersistentData.data.exp;
+
+	}
+
+	private void PlayerStatsLoad()
+    {
+		// Player persistent data UI
+		SongSelection_playerLevelUI.text = PersistentData.data.level.ToString();
+		SongSelection_playerMoneyUI.text = PersistentData.data.money.ToString();
+
+		// Player persistent data UI
+		Settings_playerLevelUI.text = PersistentData.data.level.ToString();
+		Settings_playerMoneyUI.text = PersistentData.data.money.ToString();
+
+		// Player persistent data UI
+		MainMenu_playerLevelUI.text = PersistentData.data.level.ToString();
+		MainMenu_playerMoneyUI.text = PersistentData.data.money.ToString();
+	}
 
 	int ReturnXPNeededToLevelUp(int level)
 	{
 		return (int)Mathf.Round((10 * (Mathf.Pow(level, 3))) / 5);
 	}
 
-	private void MoneyTextSetup()
+	public void SettingsButtonClicked()
     {
-		playerMoneyUI.text = PersistentData.data.money.ToString();
+		SettingsMenuPanel.SetActive(true);
+    }
 
-	}
+	public void BackButtonSettings()
+    {
+		SettingsMenuPanel.SetActive(false);
+    }
 
 
 }
