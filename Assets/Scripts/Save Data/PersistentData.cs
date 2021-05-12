591b10e385044155af3ce38b61a605b28babb1fc
Josh Morrison <josh.morrison1999@gmail.com>
2021-05-12 08:17:32 -0400

    added 50 songs to the game and fixed overlap issue with the songs author UI

--- Assets/Scripts/Save Data/PersistentData.cs
+++ Assets/Scripts/Save Data/PersistentData.cs
@@ -62,6 +62,8 @@ public class PersistentData : MonoBehaviour, ISaveable
 
         //SaveJsonData(this);             // During development Activate this function first to reset the song list
 
+        //PlayerPrefs.DeleteKey("installed"); // During development Activate this function first to reset the song list
+
         if (PlayerPrefs.HasKey("installed") == false)
         {
             SetTotalSongNotes();
